0b27d99b7672619c0fbcded71da8ce5b78b19a37
Guthen <33220603+Guthen@users.noreply.github.com>
2020-12-27 18:53:22 +0100

    💵 Money: chat command (/givemoney)

--- gamemodes/virtualroleplay/gamemode/modules/money/sh_money.lua
+++ gamemodes/virtualroleplay/gamemode/modules/money/sh_money.lua
@@ -23,3 +23,20 @@ end
 function PLAYER:CanAfford( amount )
     return self:GetMoney() >= amount
 end
+
+--  chat commands
+if not SERVER then return end
+
+VRP.AddChatCommand( "givemoney", function( ply, args )
+    local amount = tonumber( args[1] )
+    if not amount or amount <= 0 then return "Money: you must specify the amount!" end
+    if not ply:CanAfford( amount ) then return "Money: you can't give more that you have!" end
+
+    local target = ply:GetEyeTrace().Entity
+    if not IsValid( target ) or not target:IsPlayer() or target:GetPos():Distance( ply:GetPos() ) > 256 then
+        return "Money: you must look at someone or be closer!"
+    end
+
+    target:AddMoney( amount )
+    ply:AddMoney( -amount )
+end )
