67230d47b77dd775cb4eb7afee7abf6bfcabc4ae
Guthen <33220603+Guthen@users.noreply.github.com>
2020-12-27 17:45:44 +0100

    💵 Money: add format money function

--- gamemodes/virtualroleplay/gamemode/modules/money/sh_money.lua
+++ gamemodes/virtualroleplay/gamemode/modules/money/sh_money.lua
@@ -1,6 +1,16 @@
 VRP.AddPlayerNetworkVar( "Int", "Money", true )
 
---  money wrapper
+--  global variables
+VRP.MoneyAttachLeft = true
+VRP.MoneyCurrency = "$"
+
+function VRP.FormatMoney( money )
+    return ( VRP.MoneyAttachLeft and VRP.MoneyCurrency or "" )
+            .. string.Comma( money )
+            .. ( VRP.MoneyAttachLeft and "" or VRP.MoneyCurrency )
+end
+
+--  meta
 local PLAYER = FindMetaTable( "Player" )
 
 function PLAYER:AddMoney( amount )
