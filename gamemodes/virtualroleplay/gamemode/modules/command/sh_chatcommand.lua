c3977808512cc76a172b77f5c1f17f8f26ff14f2
Guthen <33220603+Guthen@users.noreply.github.com>
2020-12-27 18:43:13 +0100

    💬 Chat Commands: init

---
+++ gamemodes/virtualroleplay/gamemode/modules/command/sh_chatcommand.lua
@@ -0,0 +1,37 @@
+VRP.ChatCommands = {}
+VRP.CommandIndexor = "/"
+
+function VRP.AddChatCommand( cmd, callback )
+    VRP.ChatCommands[cmd] = callback
+end
+
+function VRP.HandleChatCommand( ply, text )
+    --  command
+    local args = text:Split( " " )
+    local cmd = table.remove( args, 1 )
+
+    if cmd and cmd:StartWith( VRP.CommandIndexor ) then
+        cmd = cmd:gsub( VRP.CommandIndexor, "" )
+        if VRP.ChatCommands[cmd] then
+            VRP.ChatCommands[cmd]( ply, args )
+            return ""
+        end
+    end
+
+    --  normal text
+    return text
+end
+
+if SERVER then
+    function GM:PlayerSay( ply, text, is_team_chat )
+        return VRP.HandleChatCommand( ply, text )
+    end
+else
+    function GM:OnPlayerChat( ply, text, is_team_chat, is_dead )
+        if ply == LocalPlayer() then
+            return not ( VRP.HandleChatCommand( ply, text ) == text )
+        end
+
+        return false
+    end
+end
