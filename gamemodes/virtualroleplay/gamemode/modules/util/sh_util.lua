096f8a41cac48c06bb8d0d2e37c7cb0b54dacaa3
Guthen <33220603+Guthen@users.noreply.github.com>
2020-12-31 13:55:38 +0100

    ⚙ Enhancement: move `modules/util/sh_util.lua` to `libraries/sh_util.lua`

--- gamemodes/virtualroleplay/gamemode/modules/util/sh_util.lua
+++ gamemodes/virtualroleplay/gamemode/modules/util/sh_util.lua
@@ -1,19 +1,3 @@
-
-function VRP.NetworkReceiveAsMethods( uint_bytes, methods )
-    return function( len, ply )
-        local method = net.ReadUInt( uint_bytes )
-        if not methods[method] then return end
-
-        methods[method]( ply or LocalPlayer(), len )
-    end
-end
-
-function VRP.Format( text, params )
-    return text:gsub( "%${([%w_%d]+)}", function( str )
-        return params[str:Trim()] or "?"
-    end )
-end
-
 --  meta
 local PLAYER = FindMetaTable( "Player" )
 
