e8ba2db6ccfc1fc432c568f7892c2cfe8f24a226
Nogitsu <nogitsu.9@gmail.com>
2020-12-31 09:40:40 +0100

    Added dynamic chat

---
+++ gamemodes/virtualroleplay/gamemode/modules/dynamic-voice/sv_chat.lua
@@ -0,0 +1,40 @@
+local tchats = {
+    { color = Color( 255, 0, 0 ), txt = "{Global} " },
+    { txt = "[Radio] " }
+}
+
+function GM:PlayerSay( ply, txt, bTeam )
+    --  > Chat commands
+    if VRP.HandleChatCommand( ply, txt ) == false then return false end
+
+    --  > OOC
+    if txt:StartWith( "//" ) then
+        for k, v in ipairs( player.GetHumans() ) do
+            v:PrintChat( team.GetColor( ply:Team() ), "(OOC) ", ply:GetName(), ": ", color_white, txt:sub( 3 ):Trim() )
+        end
+
+        return false
+    end
+
+    --  > Other talking modes
+    for k, v in ipairs( player.GetHumans() ) do
+        local can_hear, _, tchat = VRP.CanHear( v, ply )
+
+        if not can_hear then continue end
+        local team_col = team.GetColor( ply:Team() )
+
+        if ( tchat == 1 and ply:GetGlobalEars() and VRP.InHearableRadius( v, ply ) ) or ( tchat == 2 and v ~= ply ) then
+            v:PrintChat( team_col, ply:GetName(), ": ", color_white, txt )
+
+            if tchat == 1 then continue end
+        end
+
+        v:PrintChat(
+            tchats[tchat] and tchats[tchat].color or team_col,
+            tchats[tchat] and tchats[tchat].txt or "",
+            team_col, ply:GetName(), ": ", color_white, txt
+        )
+    end
+
+    return false
+end
