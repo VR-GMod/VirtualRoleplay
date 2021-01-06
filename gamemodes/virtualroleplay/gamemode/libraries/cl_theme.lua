38aa288dea999abf923029ab1817d397a0932c1a
Nogitsu <nogitsu.9@gmail.com>
2021-01-06 17:50:19 +0100

    Put colors and fonts in another folder

---
+++ gamemodes/virtualroleplay/gamemode/libraries/cl_theme.lua
@@ -0,0 +1,18 @@
+--  > Colors
+VRP.Colors = {
+    background = Color( 0, 0, 0, 175 ),
+    red = Color( 255, 71, 87 ),
+    blue = Color( 30, 144, 255 ),
+    green = Color( 46, 213, 115 )
+}
+
+--  > Fonts
+surface.CreateFont( "VRP:Font24", {
+    font = "Bahnschrift",
+    size = 24
+} )
+
+surface.CreateFont( "VRP:Font18", {
+    font = "Bahnschrift",
+    size = 18
+} )
